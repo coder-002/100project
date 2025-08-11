36eb15191a2c8cee191acb253f9e7a2399298254
coder-002 <markoshprajapati@gmail.com>
2025-08-12 00:22:23 +0545

    word count from file

---
+++ word_count/target/debug/deps/word_count-6653d063bdb1ba99.d
@@ -0,0 +1,5 @@
+/home/premium/drive/learn/rust/100project/word_count/target/debug/deps/word_count-6653d063bdb1ba99: src/main.rs
+
+/home/premium/drive/learn/rust/100project/word_count/target/debug/deps/word_count-6653d063bdb1ba99.d: src/main.rs
+
+src/main.rs:
---
+++ word_count/target/debug/word_count.d
@@ -0,0 +1,1 @@
+/home/premium/drive/learn/rust/100project/word_count/target/debug/word_count: /home/premium/drive/learn/rust/100project/word_count/src/main.rs
