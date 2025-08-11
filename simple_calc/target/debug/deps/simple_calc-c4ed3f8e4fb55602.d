88794208bd9574794f37786f75975d34c021acfb
coder-002 <markoshprajapati@gmail.com>
2025-08-12 00:00:43 +0545

    first commit

---
+++ simple_calc/target/debug/deps/simple_calc-c4ed3f8e4fb55602.d
@@ -0,0 +1,5 @@
+/home/premium/drive/learn/rust/100project/simple_calc/target/debug/deps/libsimple_calc-c4ed3f8e4fb55602.rmeta: src/main.rs
+
+/home/premium/drive/learn/rust/100project/simple_calc/target/debug/deps/simple_calc-c4ed3f8e4fb55602.d: src/main.rs
+
+src/main.rs:
---
+++ simple_calc/target/debug/deps/simple_calc-ee99878f369a1e17.d
@@ -0,0 +1,5 @@
+/home/premium/drive/learn/rust/100project/simple_calc/target/debug/deps/libsimple_calc-ee99878f369a1e17.rmeta: src/main.rs
+
+/home/premium/drive/learn/rust/100project/simple_calc/target/debug/deps/simple_calc-ee99878f369a1e17.d: src/main.rs
+
+src/main.rs:
